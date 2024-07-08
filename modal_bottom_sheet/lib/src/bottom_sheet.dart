1fa957b81cd53d17f2c0fc617f97aff7182be7a0
ArchangelXu <289203664@qq.com>
2024-07-08 14:30:00 +0800

    merge updates

--- modal_bottom_sheet/lib/src/bottom_sheet.dart
+++ modal_bottom_sheet/lib/src/bottom_sheet.dart
@@ -7,6 +7,7 @@ import 'dart:async';
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:modal_bottom_sheet/src/utils/bottom_sheet_suspended_curve.dart';
+import 'package:modal_bottom_sheet/src/utils/scroll_to_top_status_bar.dart';
 
 const Curve _decelerateEasing = Cubic(0.0, 0.0, 0.2, 1.0);
 
